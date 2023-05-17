e5f2e9046e0e254e3210d22a7842dcc01e95e1a6
samyakkhatua <samyakkhatua@duck.com>
2023-05-18 01:49:07 +0530

    component added - 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import QuizPage from "./components/QuizPage";
+import ScorePage from "./components/ScorePage";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
-}
+    <BrowserRouter>
+      <div>Samyak</div>
 
-export default App
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/score" element={<ScorePage />} />
+        <Route path="/*" element={<ScorePage />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+ 
+export default App;
---
+++ src/components/ScorePage.jsx
@@ -0,0 +1,8 @@
+
+const ScorePage = () => {
+  return (
+    <div>ScorePage</div>
+  )
+}
+
+export default ScorePage
